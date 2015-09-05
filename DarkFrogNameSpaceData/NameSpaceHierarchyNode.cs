910f8fdb9be8bc10739918e210c53dc9663b967b
Jasper Hilven <jasperhilven@gmail.com>
2015-09-05 13:07:36 +0200

    First steps of code generation

--- DarkFrogNameSpaceData/NameSpaceHierarchyNode.cs
+++ DarkFrogNameSpaceData/NameSpaceHierarchyNode.cs
@@ -8,8 +8,36 @@ namespace DarkFrogNameSpaceData
 {
   public class NameSpaceHierarchyNode
   {
-    public IEnumerable<NameSpaceHierarchyNode> Children { get; set; }
+    private NameSpaceHierarchyNode parent;
+    private IEnumerable<NameSpaceHierarchyNode> children;
+
+    public NameSpaceHierarchyNode Parent {
+      get { return parent; }
+    }
+    public IEnumerable<NameSpaceHierarchyNode> Children {
+      get { return children; }
+      set
+      {
+        children = value;
+        foreach (var nameSpaceHierarchyNode in value)
+        {
+          nameSpaceHierarchyNode.parent = this;
+        }
+      }
+    }
     public IEnumerable<NameSpaceHierarchyBareFunction> Elements { get; set; }
     public string Name { get; set; }
+
+    public List<string> Path
+    {
+      get
+      {
+        if (parent == null)
+          return new List<string>(){Name};
+        var parentPath = parent.Path;
+        parentPath.Add(Name);
+        return parentPath;
+      }
+    }
   }
 }
--- DarkFrogNameSpaceGenerator/Program.cs
+++ DarkFrogNameSpaceGenerator/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.IO;
+using DarkFrogNameSpaceData;
 namespace DarkFrogNameSpaceGenerator
 {
   class Program
@@ -11,9 +9,45 @@ namespace DarkFrogNameSpaceGenerator
     static void Main(string[] args)
     {
       var root = DarkFrogNameSpaceData.NameSpaceDataBuilder.GetRoot();
-      var path = "";
-
+      var path = @"C:\Users\Jasper\Desktop\TestGen";
+      GenerateHierachyNodeContent(path,root);
     }
 
+    private static void GenerateHierachyNodeContent(string filePath, NameSpaceHierarchyNode node)
+    {
+      var path = filePath + node.Name;
+      var pathSlashed = path + @"\";
+      Directory.Delete(path,true);
+      Directory.CreateDirectory(path);
+      foreach (var nameSpaceHierarchyNode in node.Children)
+        GenerateHierachyNodeContent(pathSlashed,nameSpaceHierarchyNode);
+      foreach (var nameSpaceHierarchyBareFunction in node.Elements)
+        GenerateBareMethodClassFile(nameSpaceHierarchyBareFunction, node, pathSlashed);
+    }
+
+
+    private static void GenerateBareMethodClassFile(NameSpaceHierarchyBareFunction bareFunction,NameSpaceHierarchyNode parent,string parentalPath)
+    {
+      var res = GenerateClassHeader(bareFunction,parent);
+      res.AddRange(GenerateMethodImplementation(bareFunction));
+      res.AddRange(GenerateClosingFile());
+      var fileName = parentalPath + bareFunction.name + ".cs";
+      File.WriteAllLines(fileName,res);
+    }
+
+    private static List<string> GenerateClassHeader(NameSpaceHierarchyBareFunction bareFunction, NameSpaceHierarchyNode parent)
+    {
+      throw new NotImplementedException();
+    }
+
+    private static List<string> GenerateMethodImplementation(NameSpaceHierarchyBareFunction bareFunction)
+    {
+      throw new NotImplementedException();
+    }
+
+    private static List<string> GenerateClosingFile()
+    {
+      throw new NotImplementedException();
+    }
   }
 }
